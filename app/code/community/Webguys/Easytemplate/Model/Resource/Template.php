538c15f392f6782313761dfe83e2051807878789
Tobias Vogt <tobias@code-x.de>
2013-08-18 15:03:34 +0200

    Ein paar Method Annotations für leichteren DB-Zugriff

--- app/code/community/Webguys/Easytemplate/Model/Resource/Template.php
+++ app/code/community/Webguys/Easytemplate/Model/Resource/Template.php
@@ -1,5 +1,24 @@
 <?php
 
+/**
+ * Class Webguys_Easytemplate_Model_Resource_Template
+ *
+ * @method getBlockId
+ * @method setBlockId
+ * @method getCode
+ * @method setCode
+ * @method getName
+ * @method setName
+ * @method getActive
+ * @method setActive
+ * @method getPosition
+ * @method setPosition
+ * @method getValidFrom
+ * @method setValidFrom
+ * @method getValidTo
+ * @method setValidTo
+ *
+ */
 class Webguys_Easytemplate_Model_Resource_Template
     extends Mage_Core_Model_Resource_Db_Abstract
 {
