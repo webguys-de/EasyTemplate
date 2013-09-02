d2c31799440f3ac2d8827cb002d866cade586297
Matthias Kleine <matthias@klein0r.de>
2013-09-02 23:49:13 +0200

    Issue #2067: Im Backend bei CMS-Pages erste Schritte - Festlegen ob Easy-Template oder Core-Funktionalität

---
+++ app/code/community/Webguys/Easytemplate/Model/Observer.php
@@ -0,0 +1,29 @@
+<?php
+
+
+class Webguys_Easytemplate_Model_Observer extends Mage_Core_Model_Abstract
+{
+    public function adminhtml_cms_page_edit_tab_main_prepare_form($observer)
+    {
+        $form = $observer->getForm();
+
+        foreach ($form->getElements() as $element) {
+            if ($element instanceof Varien_Data_Form_Element_Fieldset) {
+
+                /** @var $sourceModel Webguys_Easytemplate_Model_Config_Source_Cms_Page_Viewmode */
+                $sourceModel = Mage::getModel('easytemplate/config_source_cms_page_viewmode');
+
+                /** @var $element Varien_Data_Form_Element_Fieldset */
+                $element->addField('view_mode', 'select', array(
+                    'label'     => Mage::helper('easytemplate')->__('Mode'),
+                    'title'     => Mage::helper('easytemplate')->__('View Mode'),
+                    'name'      => 'view_mode',
+                    'required'  => true,
+                    'options'   => $sourceModel->toArray(),
+                    'note'      => Mage::helper('easytemplate')->__('Use the template engine or default behavior'),
+                    'disabled'  => false,
+                ));
+            }
+        }
+    }
+}
