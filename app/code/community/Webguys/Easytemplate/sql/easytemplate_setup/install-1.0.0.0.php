b903e1a1a230dc3242bbcc9124804fc2a225f111
Matthias Kleine <matthias@klein0r.de>
2013-11-03 19:10:44 +0100

    Better mysql-update

--- app/code/community/Webguys/Easytemplate/sql/easytemplate_setup/install-1.0.0.0.php
+++ app/code/community/Webguys/Easytemplate/sql/easytemplate_setup/install-1.0.0.0.php
@@ -8,6 +8,26 @@ $installer->startSetup();
 
 
 $table = $installer->getConnection()
+    ->newTable($installer->getTable('easytemplate/group'))
+    ->addColumn('id', Varien_Db_Ddl_Table::TYPE_SMALLINT, 6, array(
+        'identity'  => true,
+        'nullable'  => false,
+        'primary'   => true,
+    ), 'Id')
+    ->addColumn('entity_type', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
+        'nullable'  => false,
+    ), 'entity_type')
+    ->addColumn('entity_id', Varien_Db_Ddl_Table::TYPE_SMALLINT, 6, array(
+        'unsigned'  => true,
+        'nullable'  => false,
+    ), 'entity_id')
+
+    ->setComment('Templategroup');
+$installer->getConnection()->createTable($table);
+
+// --------------------------------------------------------------------
+
+$table = $installer->getConnection()
     ->newTable($installer->getTable('easytemplate/template'))
     ->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
         'identity'  => true,
@@ -15,56 +35,29 @@ $table = $installer->getConnection()
         'nullable'  => false,
         'primary'   => true,
     ), 'Id')
-    ->addColumn('group_id', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
+    ->addColumn('group_id', Varien_Db_Ddl_Table::TYPE_SMALLINT, 6, array(
     ), 'group_id')
     ->addColumn('code', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
     ), 'code')
     ->addColumn('name', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
     ), 'name')
-    ->addColumn('active', Varien_Db_Ddl_Table::TYPE_SMALLINT, 255, array(
+    ->addColumn('active', Varien_Db_Ddl_Table::TYPE_SMALLINT, 6, array(
     ), 'active')
-    ->addColumn('position', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+    ->addColumn('position', Varien_Db_Ddl_Table::TYPE_INTEGER, 10, array(
+        'unsigned'  => true,
     ), 'position')
     ->addColumn('valid_from', Varien_Db_Ddl_Table::TYPE_DATETIME, null, array(
     ), 'valid_from')
     ->addColumn('valid_to', Varien_Db_Ddl_Table::TYPE_DATETIME, null, array(
     ), 'valid_to')
 
-    /* TODO:
-    ->addIndex($installer->getIdxName('poll/poll', array('store_id')),
-        array('store_id'))
-    ->addForeignKey($installer->getFkName('poll/poll', 'store_id', 'core/store', 'store_id'),
-        'store_id', $installer->getTable('core/store'), 'store_id',
-        Varien_Db_Ddl_Table::ACTION_CASCADE,
-        Varien_Db_Ddl_Table::ACTION_CASCADE)
-    */
+    ->addIndex($installer->getIdxName('easytemplate/template', array('code')),
+        array('code'))
+    ->addForeignKey($installer->getFkName('easytemplate/template', 'group_id', 'easytemplate/group', 'id'),
+        'group_id', $installer->getTable('easytemplate/group'), 'id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE)
+
     ->setComment('Template');
-$installer->getConnection()->createTable($table);
-
-// --------------------------------------------------------------------
-
-$table = $installer->getConnection()
-    ->newTable($installer->getTable('easytemplate/group'))
-    ->addColumn('id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
-        'identity'  => true,
-        'unsigned'  => true,
-        'nullable'  => false,
-        'primary'   => true,
-    ), 'Id')
-    ->addColumn('entity_type', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
-    ), 'entity_type')
-    ->addColumn('entity_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
-    ), 'entity_id')
-
-    /* TODO:
-    ->addIndex($installer->getIdxName('poll/poll', array('store_id')),
-        array('store_id'))
-    ->addForeignKey($installer->getFkName('poll/poll', 'store_id', 'core/store', 'store_id'),
-        'store_id', $installer->getTable('core/store'), 'store_id',
-        Varien_Db_Ddl_Table::ACTION_CASCADE,
-        Varien_Db_Ddl_Table::ACTION_CASCADE)
-    */
-    ->setComment('Templategroup');
 $installer->getConnection()->createTable($table);
 
 // --------------------------------------------------------------------
@@ -77,21 +70,21 @@ $table = $installer->getConnection()
         'nullable'  => false,
         'primary'   => true,
     ), 'Id')
-    ->addColumn('element_id', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
+    ->addColumn('element_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
     ), 'block_id')
     ->addColumn('field', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
     ), 'field')
     ->addColumn('value', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
     ), 'value')
 
-    /* TODO:
-    ->addIndex($installer->getIdxName('poll/poll', array('store_id')),
-        array('store_id'))
-    ->addForeignKey($installer->getFkName('poll/poll', 'store_id', 'core/store', 'store_id'),
-        'store_id', $installer->getTable('core/store'), 'store_id',
-        Varien_Db_Ddl_Table::ACTION_CASCADE,
-        Varien_Db_Ddl_Table::ACTION_CASCADE)
-    */
+    ->addIndex($installer->getIdxName('easytemplate/template_data_varchar', array('field')),
+        array('field'))
+    ->addForeignKey($installer->getFkName('easytemplate/template_data_varchar', 'element_id', 'easytemplate/template', 'id'),
+        'element_id', $installer->getTable('easytemplate/template'), 'id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE)
+
     ->setComment('Template Data Varchar');
 $installer->getConnection()->createTable($table);
 
--- app/code/community/Webguys/Easytemplate/sql/easytemplate_setup/mysql4-upgrade-1.0.0.1-1.0.0.2.php
+++ app/code/community/Webguys/Easytemplate/sql/easytemplate_setup/mysql4-upgrade-1.0.0.1-1.0.0.2.php
@@ -15,21 +15,21 @@ $table = $installer->getConnection()
         'nullable'  => false,
         'primary'   => true,
     ), 'Id')
-    ->addColumn('element_id', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
+    ->addColumn('element_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+        'unsigned'  => true,
+        'nullable'  => false,
     ), 'block_id')
     ->addColumn('field', Varien_Db_Ddl_Table::TYPE_TEXT, 255, array(
     ), 'field')
     ->addColumn('value', Varien_Db_Ddl_Table::TYPE_TEXT, null, array(
     ), 'value')
 
-    /* TODO:
-    ->addIndex($installer->getIdxName('poll/poll', array('store_id')),
-        array('store_id'))
-    ->addForeignKey($installer->getFkName('poll/poll', 'store_id', 'core/store', 'store_id'),
-        'store_id', $installer->getTable('core/store'), 'store_id',
-        Varien_Db_Ddl_Table::ACTION_CASCADE,
-        Varien_Db_Ddl_Table::ACTION_CASCADE)
-    */
+    ->addIndex($installer->getIdxName('easytemplate/template_data_text', array('field')),
+        array('field'))
+    ->addForeignKey($installer->getFkName('easytemplate/template_data_text', 'element_id', 'easytemplate/template', 'id'),
+        'element_id', $installer->getTable('easytemplate/template'), 'id',
+        Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE)
+
     ->setComment('Template Data Text');
 $installer->getConnection()->createTable($table);
 
