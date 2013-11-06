fe6f1f27516be431dc87726b52f11984d82fba17
Ingo Hillebrand <ingo@shift8.de>
2013-11-06 20:14:04 +0100

    auswahl div templates

--- app/code/community/Webguys/Easytemplate/Model/Config/Source/Template/Type.php
+++ app/code/community/Webguys/Easytemplate/Model/Config/Source/Template/Type.php
@@ -38,4 +38,38 @@ class Webguys_Easytemplate_Model_Config_Source_Template_Type
 
         return $categories;
     }
+
+    public function toSelectionArray()
+    {
+        $categories = array();
+
+        $helper = Mage::helper('easytemplate');
+
+        /** @var $configModel Webguys_Easytemplate_Model_Input_Parser */
+        $configModel = Mage::getSingleton('easytemplate/input_parser');
+        $config = $configModel->getXmlConfig();
+
+        foreach ($config->getNode(self::TEMPLATE_TYPES_PATH)->children() as $category) {
+            $types = array();
+            $templatesPath = self::TEMPLATE_TYPES_PATH . '/' . $category->getName() . '/templates';
+
+            foreach ($config->getNode($templatesPath)->children() as $template) {
+                $labelPath = self::TEMPLATE_TYPES_PATH . '/' . $category->getName() . '/templates/' . $template->getName() . '/label';
+                $types[] = array(
+                    'label' => $helper->__((string) $config->getNode($labelPath)),
+                    'value' => $category->getName().'_'.$template->getName(),
+                    'image' => (string)$template->image
+                );
+            }
+
+            $labelPath = self::TEMPLATE_TYPES_PATH . '/' . $category->getName() . '/label';
+
+            $categories[] = array(
+                'label' => $helper->__((string) $config->getNode($labelPath)),
+                'value' => $types
+            );
+        }
+
+        return $categories;
+    }
 }
