4a090fb14aecdf17a643bc344cb6090a6a990a28
Matthias Kleine <matthias@klein0r.de>
2013-11-07 22:40:56 +0100

    Unique Child names in frontend

--- app/code/community/Webguys/Easytemplate/Block/Frontend/Abstract.php
+++ app/code/community/Webguys/Easytemplate/Block/Frontend/Abstract.php
@@ -7,6 +7,8 @@ class Webguys_Easytemplate_Block_Frontend_Abstract extends Mage_Core_Block_Templ
         /** @var $configModel Webguys_Easytemplate_Model_Input_Parser */
         $configModel = Mage::getSingleton('easytemplate/input_parser');
 
+        $position = 1;
+
         /** @var $template Webguys_Easytemplate_Model_Template */
         foreach ($group->getTemplateCollection() as $template) {
             if ($model = $configModel->getTemplate( $template->getCode() )) {
@@ -18,7 +20,8 @@ class Webguys_Easytemplate_Block_Frontend_Abstract extends Mage_Core_Block_Templ
                     $childBlock->setData($field->getCode(), $template->getFieldData($field->getCode()));
                 }
 
-                $this->setChild('block_'.$template->getCode(), $childBlock);
+                $this->setChild('block_'.$position.'_'.$template->getCode(), $childBlock);
+                $position++;
             }
         }
     }
