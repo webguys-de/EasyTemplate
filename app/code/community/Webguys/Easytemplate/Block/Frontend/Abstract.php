ea095c2f722a2822f40a29cdf72ee7d7e5cadb2f
Matthias Kleine <matthias@klein0r.de>
2013-11-07 22:32:58 +0100

    Finished frontend for page

--- app/code/community/Webguys/Easytemplate/Block/Frontend/Abstract.php
+++ app/code/community/Webguys/Easytemplate/Block/Frontend/Abstract.php
@@ -7,12 +7,16 @@ class Webguys_Easytemplate_Block_Frontend_Abstract extends Mage_Core_Block_Templ
         /** @var $configModel Webguys_Easytemplate_Model_Input_Parser */
         $configModel = Mage::getSingleton('easytemplate/input_parser');
 
+        /** @var $template Webguys_Easytemplate_Model_Template */
         foreach ($group->getTemplateCollection() as $template) {
             if ($model = $configModel->getTemplate( $template->getCode() )) {
                 $childBlock = $this->getLayout()->createBlock($model->getType());
                 $childBlock->setTemplate($model->getTemplate());
 
-                //$childBlock->setData();
+                /** @var $field Webguys_Easytemplate_Model_Input_Parser_Field */
+                foreach ($model->getFields() as $field) {
+                    $childBlock->setData($field->getCode(), $template->getFieldData($field->getCode()));
+                }
 
                 $this->setChild('block_'.$template->getCode(), $childBlock);
             }
