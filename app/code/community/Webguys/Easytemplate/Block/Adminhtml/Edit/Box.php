38337bd7168d3a4252f7feabace901121365a773
Ingo Hillebrand <ingo@shift8.de>
2013-11-09 00:19:12 +0100

    Pimped Darstellung Baustein

--- app/code/community/Webguys/Easytemplate/Block/Adminhtml/Edit/Box.php
+++ app/code/community/Webguys/Easytemplate/Block/Adminhtml/Edit/Box.php
@@ -17,25 +17,70 @@ class Webguys_Easytemplate_Block_Adminhtml_Edit_Box
         $this->setTemplate('easytemplate/edit/box.phtml');
     }
 
+    public function getOpenButton($id)
+    {
+        $button = $this->getLayout()->createBlock('adminhtml/widget_button')
+            ->setData(array(
+                'id' => 'atwixtweaks_button',
+                'label' => $this->helper('easytemplate')->__('Edit'),
+                'onclick' => '$(\'template_content_' . $id . '\').show();$(\'template_overview_' . $id . '\').hide();',
+                'class' => 'scalable back',
+                'title' => $this->helper('easytemplate')->__('Edit'),
+                'style' => 'margin-top: 5px;'
+            ));
+        return $button->toHtml();
+    }
+
+    public function getCloseButton($id)
+    {
+        $button = $this->getLayout()->createBlock('adminhtml/widget_button')
+            ->setData(array(
+                'id' => 'atwixtweaks_button',
+                'label' => $this->helper('easytemplate')->__('Close'),
+                'onclick' => '$(\'template_content_' . $id . '\').hide();$(\'template_overview_' . $id . '\').show();',
+                'class' => 'scalable back',
+                'title' => $this->helper('easytemplate')->__('Close')
+            ));
+        return $button->toHtml();
+    }
+
+    public function getDatePicker()
+    {
+
+        $ret = <<<EOF
+        <script type="text/javascript">
+            //<![CDATA[
+            Calendar.setup({
+                inputField : '_dob',
+                ifFormat : '%m/%e/%y',
+                button : '_dob_trig',
+                align : 'Bl',
+                singleClick : true
+            });
+            //]]>
+        </script>
+EOF;
+    }
+
     public function getDeleteButtonHtml()
     {
         return $this->getChildHtml('delete_button');
     }
 
-    public function _toHtml() {
+    public function _toHtml()
+    {
 
         /** @var $input Webguys_Easytemplate_Block_Adminhtml_Edit_Renderer */
         $input = $this->getLayout()->createBlock('easytemplate/adminhtml_edit_renderer');
-        $input->setTemplateModel( $this->getTemplateModel() );
+        $input->setTemplateModel($this->getTemplateModel());
         $this->setChild('input', $input);
 
         $html = parent::_toHtml();
 
-        if ( $this->getTemplateModel() && $this->getTemplateModel()->getId() ) {
+        if ($this->getTemplateModel() && $this->getTemplateModel()->getId()) {
 
-            foreach( $this->getTemplateModel()->getData() AS $replace => $to )
-            {
-                $html = str_replace('{{'.$replace.'}}', $to, $html);
+            foreach ($this->getTemplateModel()->getData() AS $replace => $to) {
+                $html = str_replace('{{' . $replace . '}}', $to, $html);
             }
 
         }
@@ -64,7 +109,7 @@ class Webguys_Easytemplate_Block_Adminhtml_Edit_Box
         return $this->_template_model;
     }
 
-    public function setTemplateModel( Webguys_Easytemplate_Model_Template $model )
+    public function setTemplateModel(Webguys_Easytemplate_Model_Template $model)
     {
         $this->_template_model = $model;
         return $this;
