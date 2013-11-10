1b8833e8c24c97896ba6ef0b57bbea86f40489d6
Matthias Kleine <matthias@klein0r.de>
2013-11-10 21:59:02 +0100

    - Added int, decimal and datetime tables

---
+++ app/code/community/Webguys/Easytemplate/Model/Resource/Template/Data/Decimal/Collection.php
@@ -0,0 +1,11 @@
+<?php
+
+class Webguys_Easytemplate_Model_Resource_Template_Data_Decimal_Collection
+    extends Webguys_Easytemplate_Model_Resource_Template_Data_Collection_Abstract
+{
+    protected function _construct()
+    {
+        $this->_init('easytemplate/template_data_decimal');
+    }
+
+}
