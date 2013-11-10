19bc2076a565aefd5f8409e6c812240cb7b5477d
Matthias Kleine <matthias@klein0r.de>
2013-11-10 22:30:50 +0100

    #5: Eingabefeld File

--- app/code/community/Webguys/Easytemplate/Block/Input/Renderer/File.php
+++ app/code/community/Webguys/Easytemplate/Block/Input/Renderer/File.php
@@ -7,4 +7,21 @@ class Webguys_Easytemplate_Block_Input_Renderer_File extends Webguys_Easytemplat
         parent::__construct();
         $this->setTemplate('easytemplate/input/renderer/file.phtml');
     }
+
+    public function hasFile()
+    {
+        return isset($this->_value);
+    }
+
+    public function isImageFile()
+    {
+        // TODO: Determine if value is image
+        return false;
+    }
+
+    public function getFileUrl()
+    {
+        // TODO: Get file url
+        return Mage::getUrl('*/*/*');
+    }
 }
