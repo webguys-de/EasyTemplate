21b5360c3fa4eaf29c9737d526557ac5f7380817
Matthias Kleine <matthias@klein0r.de>
2013-11-18 21:16:48 +0100

    Added field validator events

--- app/code/community/Webguys/Easytemplate/Model/Input/Renderer/Validator/Base.php
+++ app/code/community/Webguys/Easytemplate/Model/Input/Renderer/Validator/Base.php
@@ -12,4 +12,36 @@ class Webguys_Easytemplate_Model_Input_Renderer_Validator_Base extends Mage_Core
     {
         return $data;
     }
+
+    /**
+     * @param $backendModel Webguys_Easytemplate_Model_Template_Data_Abstract
+     * @param $field Webguys_Easytemplate_Model_Input_Parser_Field
+     * @param $value mixed
+     */
+    public function beforeFieldSave($backendModel, $field, $value)
+    {
+        Mage::dispatchEvent('easytemplate_backend_field_save_before', array(
+            'backend_model' => $backendModel,
+            'field' => $field,
+            'value' => $value
+        ));
+
+        return $this;
+    }
+
+    /**
+     * @param $backendModel Webguys_Easytemplate_Model_Template_Data_Abstract
+     * @param $field Webguys_Easytemplate_Model_Input_Parser_Field
+     * @param $value mixed
+     */
+    public function afterFieldSave($backendModel, $field, $value)
+    {
+        Mage::dispatchEvent('easytemplate_backend_field_save_after', array(
+            'backend_model' => $backendModel,
+            'field' => $field,
+            'value' => $value
+        ));
+
+        return $this;
+    }
 }
