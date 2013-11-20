21e6b6aeb429bb863eef82c86977038f4f135e71
Tobias Vogt <tobias@code-x.de>
2013-11-20 23:05:26 +0100

    Link - Categories and Pages

--- skin/adminhtml/default/default/easytemplate/chooser.js
+++ skin/adminhtml/default/default/easytemplate/chooser.js
@@ -5,9 +5,12 @@ EasytemplateChooser.prototype = {
     initialize: function( chooser, config ) {
 
         this.chooser = chooser;
-        this.product_chooser_url = config.product_chooser_url;
         this.title = config.title;
         this.button_close = config.button_close;
+
+        this.product_chooser_url = config.product_chooser_url;
+        this.category_chooser_url = config.category_chooser_url;
+        this.cms_chooser_url = config.cms_chooser_url;
 
     },
 
@@ -17,13 +20,19 @@ EasytemplateChooser.prototype = {
         if( entity == 'product' )
         {
             url = this.product_chooser_url;
+        } else if( entity == 'category' )
+        {
+            url = this.category_chooser_url;
+        } else if( entity == 'cms' )
+        {
+            url = this.cms_chooser_url;
         }
-
 
         this.chooser.chooserUrl = url;
         this.chooser.config.buttons = {"open": this.title ,"close": this.button_close };
 
 
+        this.chooser.dialogContent = null;
         this.chooser.choose();
 
     }
