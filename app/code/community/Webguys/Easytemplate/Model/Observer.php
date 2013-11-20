21724b39037cc1d39ed43889843698682efaffd5
Matthias Kleine <matthias@klein0r.de>
2013-11-21 00:53:49 +0100

    Add view mode to page grid

--- app/code/community/Webguys/Easytemplate/Model/Observer.php
+++ app/code/community/Webguys/Easytemplate/Model/Observer.php
@@ -89,4 +89,29 @@ class Webguys_Easytemplate_Model_Observer extends Mage_Core_Model_Abstract
         }
 
     }
+
+    public function adminhtml_block_html_before($observer)
+    {
+        $block = $observer->getEvent()->getBlock();
+        if ($block instanceof Mage_Adminhtml_Block_Cms_Page_Grid) {
+
+            /** @var $sourceModel Webguys_Easytemplate_Model_Config_Source_Cms_Page_Viewmode */
+            $sourceModel = Mage::getModel('easytemplate/config_source_cms_page_viewmode');
+
+            $block->addColumnAfter(
+                'view_mode',
+                array(
+                    'header' => Mage::helper('easytemplate')->__('Mode'),
+                    'index' => 'view_mode',
+                    'width' => '100px',
+                    'header_css_class' => 'view_mode',
+                    'sortable' => true,
+                    'type' => 'options',
+                    'options'  => $sourceModel->toArray(false),
+                ),
+                'root_template'
+            );
+
+        }
+    }
 }
