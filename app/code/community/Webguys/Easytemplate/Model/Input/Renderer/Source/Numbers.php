fe86c38813c0204f04304c149ce335f3d400c6a6
Ingo Hillebrand <ingo@shift8.de>
2013-11-23 15:36:11 +0100

    #11: Einige sinnvolle Basis-Vorlagen erstellen

---
+++ app/code/community/Webguys/Easytemplate/Model/Input/Renderer/Source/Numbers.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Class Webguys_Easytemplate_Model_Input_Renderer_Source_Yesno
+ *
+ */
+class Webguys_Easytemplate_Model_Input_Renderer_Source_Numbers extends Webguys_Easytemplate_Model_Input_Renderer_Source_Abstract
+{
+    public function getOptionValues()
+    {
+        $values = array();
+        for($i=0;$i<51;$i++){
+            $values[$i] = $i;
+        }
+        return $this->translateOptions($values);
+    }
+}
