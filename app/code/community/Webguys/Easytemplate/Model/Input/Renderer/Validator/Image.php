1b2322f1650e5e887e9bfcbc973892e0f374400d
Matthias Kleine <matthias@klein0r.de>
2015-07-22 22:14:33 +0200

    Image frontend prepare

---
+++ app/code/community/Webguys/Easytemplate/Model/Input/Renderer/Validator/Image.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Class Webguys_Easytemplate_Model_Input_Renderer_Validator_Image
+ *
+ */
+class Webguys_Easytemplate_Model_Input_Renderer_Validator_Image extends Webguys_Easytemplate_Model_Input_Renderer_Validator_Base
+{
+
+    public function prepareForFrontend($data)
+    {
+        if (empty($data)) {
+            return $data;
+        }
+
+        return Mage::app()->getStore()->getBaseUrl(Mage_Core_Model_Store::URL_TYPE_MEDIA) . DS . $data;
+    }
+
+}
