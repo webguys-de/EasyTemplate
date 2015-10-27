1cbfe7229da5e8d0ba3f2d32af44f45077c8500c
Matthias Kleine <matthias@klein0r.de>
2015-10-27 20:28:20 +0100

    Category backend model fix

--- app/code/community/Webguys/Easytemplate/Block/Input/Renderer/Category.php
+++ app/code/community/Webguys/Easytemplate/Block/Input/Renderer/Category.php
@@ -15,7 +15,7 @@ class Webguys_Easytemplate_Block_Input_Renderer_Category extends Webguys_Easytem
 
     public function getDefaultBackendModel()
     {
-        return Mage::getModel('easytemplate/template_data_int');
+        return Mage::getModel('easytemplate/template_data_varchar');
     }
 
     /**
