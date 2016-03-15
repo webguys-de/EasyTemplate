d8fd42308c808780f97b1f64d806e08d49766c38
Gerhard Fobe <gerhard.fobe@mscg.de>
2016-03-15 14:32:11 +0100

    add preparation for categories in frontend (analog to the product way)

--- app/code/community/Webguys/Easytemplate/Model/Input/Renderer/Validator/Category.php
+++ app/code/community/Webguys/Easytemplate/Model/Input/Renderer/Validator/Category.php
@@ -6,6 +6,14 @@
  */
 class Webguys_Easytemplate_Model_Input_Renderer_Validator_Category extends Webguys_Easytemplate_Model_Input_Renderer_Validator_Base
 {
+    public function prepareForFrontend($data)
+    {
+        /** @var $product Mage_Catalog_Model_Category */
+        $category = Mage::getModel('catalog/category');
+        $category->load($data);
+
+        return ($category->getId()) ? $category : false;
+    }
 
     public function prepareForSave($data)
     {
